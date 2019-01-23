686b417dfabbc9d6c9ced74a2cb77e2ca3cb34f4
BAREz98 <46967665+BAREz98@users.noreply.github.com>
2019-01-23 16:58:42 +0100

    Update SoftwareSerialExample.ino

--- SoftwareSerialExample.ino
+++ SoftwareSerialExample.ino
@@ -48,8 +48,8 @@ void loop() { // run over and over
   if (mySerial.available()) {
     Serial.write(mySerial.read());
   }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
+  else (Serial.available()) {
+    mySerial.write(MSerial.read());
   }
 }
 
